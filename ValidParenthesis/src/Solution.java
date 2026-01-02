e02304fea3dac753363ad36533c6e49d9e2b512b
Lynnece Eutsey <lynxe-dev@outlook.com>
2026-01-02 10:16:25 -0600

    solution completed

--- ValidParenthesis/src/Solution.java
+++ ValidParenthesis/src/Solution.java
@@ -1,2 +1,32 @@
+import java.util.Stack;
+
 public class Solution {
+
+    public boolean isValid(String s){
+
+        Stack<Character> stack = new Stack<>();
+
+        char[] charArray = s.toCharArray();
+        for(int i = 0; i < charArray.length; i++) {
+            char c = charArray[i];
+
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty())
+                    return false;
+
+                char top = stack.pop();
+                if (
+                        (c == ')' && top != '(') ||
+                                (c == '}' && top != '{') ||
+                                (c == ']' && top != '[')
+                ) {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
 }
